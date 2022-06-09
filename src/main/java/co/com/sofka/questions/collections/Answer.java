04230a61b079ce82f6157155a36280a141825f1b
Jorge Reyes <reyesjorge0116@gmail.com>
2022-06-09 00:17:34 -0500

    Se agrega lombok, pero se debe modificar el mapper

--- src/main/java/co/com/sofka/questions/collections/Answer.java
+++ src/main/java/co/com/sofka/questions/collections/Answer.java
@@ -1,9 +1,15 @@
 package co.com.sofka.questions.collections;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "answers")
 public class Answer {
     @Id
     private String id;
@@ -11,44 +17,4 @@ public class Answer {
     private String questionId;
     private String answer;
     private Integer position;
-
-    public Integer getPosition() {
-        return position;
-    }
-
-    public void setPosition(Integer position) {
-        this.position = position;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(String questionId) {
-        this.questionId = questionId;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
 }
--- src/main/java/co/com/sofka/questions/collections/Question.java
+++ src/main/java/co/com/sofka/questions/collections/Question.java
@@ -1,10 +1,15 @@
 package co.com.sofka.questions.collections;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
-@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "questions")
 public class Question {
     @Id
     private String id;
@@ -12,45 +17,4 @@ public class Question {
     private String question;
     private String type;
     private String category;
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
 }
